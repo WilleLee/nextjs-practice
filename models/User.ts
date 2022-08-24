4952648a50c9a50b64a760bf65d4a0eec306a6fe
Wille Lee <1992season@gmail.com>
2022-08-24 19:06:14 +0900

    add bcrypt in user model

--- models/User.ts
+++ models/User.ts
@@ -1,10 +1,18 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema({
   firstname: { type: String, required: true, minlength: 2, maxlength: 15 },
   lastname: { type: String, required: true, minlength: 2, maxlength: 15 },
   useremail: { type: String, required: true },
   password: { type: String, required: true, minlength: 8 },
+});
+
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
 });
 
 export default mongoose.models.User || mongoose.model("User", userSchema);
